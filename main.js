6f90af27be2aa4c172c5ef145d0793eb8e02ee44
kuesa <me@noahgrove.me>
2018-08-21 12:57:14 -0700

    Change from npm to yarn, added renderer.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+//Electron requirements
 const {app, BrowserWindow} = require('electron')
 
+//make sure the main window doesnt just close
 let mainWindow
 
+//create initial window
 function createWindow() {
     mainWindow = new BrowserWindow({width: 800, height:600})
 
@@ -12,8 +15,10 @@ function createWindow() {
     })
 }
 
+//create window when ready
 app.on('ready', createWindow)
 
+//when closed, close, except for the mac people
 app.on('window-all-closed', function(){
 
     if(process.platform !== 'darwin') {
@@ -21,8 +26,9 @@ app.on('window-all-closed', function(){
     }
 })
 
+//mac people opening programs in a silly way
 app.on('activate', function() {
-    
+
     if(mainWindow == null) {
         createWindow()
     }
---
+++ renderer.js
@@ -0,0 +1,1 @@
+//renderererrer file
